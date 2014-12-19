bc0aaf88f443692f99dece7a2d7e83c2e1ab5ff4
Tom Coleman <tom@thesnail.org>
2014-12-19 17:39:31 +1100

    Slightly more robust and message on how to use it.

--- outliner.rb
+++ outliner.rb
@@ -1,20 +1,26 @@
 #!/usr/bin/ruby
 
+# Cd to the DM_en repo, then run
+#
+#   for file in *.md.erb ;do ../outliner/outliner.rb < "$file" > "../DiscoverMeteor_outline/$file"; done
+#
+# (assuming things are in the right spot)
+
 inMetadata = false
 inComment = false
 
 STDIN.each do |line|
-  if line =~ /^---/
+  if line =~ /^\s*---/
     inMetadata = ! inMetadata
-  elsif line =~ /^~~~/
+  elsif line =~ /^\s*~~~/
     inComment = ! inComment
   elsif not inMetadata and not inComment
     # not a tag, blank or a heading
-    if line !~ /^<%/ and line.strip != '' and line !~ /^#/
-      if line =~ /^- /
-        puts '- //'
+    if line !~ /^\s*<%/ and line.strip != '' and line !~ /^#/
+      if line =~ /^\s*- /
+        puts '- ////'
       else
-        puts '//'
+        puts '////'
       end
       next # don't print out this line
     end
